7e4c4336e40a2229041fcf51cb93313219df3d54
Justin Mancherje <mancherje.justin@gmail.com>
2016-05-31 12:59:29 -0700

    update dependencies and utilize redux provider and react-router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import { Router, browserHistory } from 'react-router'
+import { createStore, applyMiddleware } from 'redux'
+import promise from 'redux-promise'
+
+import reducers from './reducers'
 import App from './components/App'
 import ListView from './components/ListView'
 import routes from './routes'
 
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore)
+const store = createStoreWithMiddleware(reducers)
+
 ReactDOM.render(
-  <Router history={browserHistory} routes={routes} />,
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>,
   document.querySelector('#main')
 )
