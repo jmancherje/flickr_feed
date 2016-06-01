94bdc7c34794488f891e2266ce0b9645d3f1001c
Justin Mancherje <mancherje.justin@gmail.com>
2016-05-31 19:05:40 -0700

    set up interval to update feed every 1 minute and show an alert

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import ListView from './ListView'
 import { Link } from 'react-router'
+import { updateFeed } from '../actions'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      showAlert: false
+    }
+  }
+
+  componentDidMount() {
+    this.fetchEveryMinute = window.setInterval(this.fetchAndAlert.bind(this), 60000)
+  }
+
+  fetchAndAlert() {
+    const self = this
+    this.props.updateFeed()
+      .then(() => {
+        self.toggleAlert()
+        window.setTimeout(self.toggleAlert.bind(this), 10000)
+      })
+  }
+
+  toggleAlert() {
+    console.log('open close..')
+    this.setState({ showAlert: !this.state.showAlert })
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.fetchEveryMinute)
+  }
+
   render() {
     return (
       <div>
         <Link to="/feed">feed</Link>
         <Link to="/favorites">favorites</Link>
-        <h4>Hello</h4>
+        <div className={`alert alert-info ${this.state.showAlert ? '' : 'hidden-xs-up'}`} role="alert">
+          <strong>Awesome!</strong> New images in your feed. Check the last page or reverse the order to see what's new!
+        </div>
         {this.props.children}
       </div>
     )
   }
 }
 
-export default App
+export default connect(null, { updateFeed })(App)
