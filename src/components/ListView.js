e3305e32db7e9f71cec4db126077383724c9b1b9
Justin Mancherje <mancherje.justin@gmail.com>
2016-06-02 12:18:21 -0700

    add functionalty to test if image is portrait or landscape in listview and add styles to index.html temporarily

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { updateFeed } from '../actions'
+import ImageCard from './ImageCard'
 import Pagination from './Pagination'
 
 class ListView extends Component {
@@ -8,11 +9,28 @@ class ListView extends Component {
     this.props.updateFeed()
   }
 
+  determinePortrait(image) {
+    const description = image.description
+    const startWidth = description.indexOf('width="') + 7
+    const endWidth = description.indexOf('"', startWidth)
+    const width = +description.substring(startWidth, endWidth)
+    console.log(width)
+
+    const startHeight = description.indexOf('height="') + 8
+    const endHeight = description.indexOf('"', startHeight)
+    const height = +description.substring(startHeight, endHeight)
+    console.log(height)
+
+    const aspectRatio = width / height
+    return aspectRatio < 1.333333333
+  }
+
   renderImages() {
     const page = this.props.pageNumber - 1
+    const self = this
     return !this.props.images[page] ? null :
       this.props.images[page].map((item, index) => 
-        <img key={index} src={item.media.m} height="150px" />
+        <ImageCard viewImage={'hi'} imageData={item} key={index} portrait={self.determinePortrait(item)} />
       )
   }
 
