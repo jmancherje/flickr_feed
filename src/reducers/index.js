c0270de0b83b0c6712e5423e0650146a355dcbca
Justin Mancherje <mancherje.justin@gmail.com>
2016-06-03 14:32:08 -0700

    move auth HOC to components folder, create auth reducer and attach to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ import images from './images_reducer'
 import favorites from './favorites_reducer'
 import ui from './ui_reducer'
 import currentImage from './current_image_reducer'
+import auth from './auth_reducer'
 
 export default combineReducers({
   images,
   favorites,
   currentImage,
-  ui
+  ui,
+  auth
 })
