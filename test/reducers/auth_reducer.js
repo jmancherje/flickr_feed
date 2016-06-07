df828cb5b2f446448204b144f5be48cbee3f737c
Justin Mancherje <mancherje.justin@gmail.com>
2016-06-06 19:56:31 -0700

    auth reducer fully tested

---
+++ test/reducers/auth_reducer.js
@@ -0,0 +1,107 @@
+import { expect } from 'chai'
+
+import reducer from '../../src/reducers/auth_reducer'
+import { AUTH_USER, AUTH_ERROR, UNAUTH_USER } from '../../src/actions/types'
+
+
+describe('auth reducer', () => {
+
+  describe('AUTH_USER', () => {
+    it('initially sets user authenticated to true', () => {
+      const initialState = {}
+      const action = { type: AUTH_USER }
+
+      const nextState = reducer(initialState, action)
+
+      expect(nextState).to.deep.equal({ 
+        authenticated: true,
+        error: ''
+      })
+    })
+
+    it('changes authenticated: false to true', () => {
+      const initialState = { authenticated: false }
+      const action = { type: AUTH_USER }
+
+      const nextState = reducer(initialState, action)
+
+      expect(nextState).to.deep.equal({ 
+        authenticated: true,
+        error: ''
+      })
+    })
+
+    it('clears any existing auth error message', () => {
+      const initialState = { authenticated: false, error: 'invalid login credentials' }
+      const action = { type: AUTH_USER }
+
+      const nextState = reducer(initialState, action)
+
+      expect(nextState).to.deep.equal({ 
+        authenticated: true,
+        error: ''
+      })
+    })    
+  })
+
+  describe('UNAUTH_USER', () => {
+    it('changes authenticated: true to false', () => {
+      const initialState = { authenticated: true }
+      const action = { type: UNAUTH_USER }
+
+      const nextState = reducer(initialState, action)
+
+      expect(nextState).to.deep.equal({ 
+        authenticated: false,
+        error: ''
+      })
+    })
+
+    it('clears any existing auth error message', () => {
+      const initialState = { authenticated: false, error: 'invalid login credentials' }
+      const action = { type: UNAUTH_USER }
+
+      const nextState = reducer(initialState, action)
+
+      expect(nextState).to.deep.equal({ 
+        authenticated: false,
+        error: ''
+      })
+    })    
+  })
+
+  describe('AUTH_ERROR', () => {
+    it('creates an auth error message', () => {
+      const initialState = {}
+      const action = { type: AUTH_ERROR, errorMessage: 'email already exists' }
+
+      const nextState = reducer(initialState, action)
+
+      expect(nextState).to.deep.equal({
+        error: 'email already exists'
+      })
+    })
+
+    it('overrides an existing auth error message', () => {
+      const initialState = { error: 'invalid login credentials' }
+      const action = { type: AUTH_ERROR, errorMessage: 'email already exists' }
+
+      const nextState = reducer(initialState, action)
+
+      expect(nextState).to.deep.equal({
+        error: 'email already exists'
+      })
+    })
+
+    it('doesn\'t change authenticated state', () => {
+      const initialState = { authenticated: false, error: '' }
+      const action = { type: AUTH_ERROR, errorMessage: 'email already exists' }
+
+      const nextState = reducer(initialState, action)
+
+      expect(nextState).to.deep.equal({ error: 'email already exists', authenticated: false })
+    })
+  })
+
+})
+
