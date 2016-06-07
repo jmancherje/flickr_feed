6922506d8e3dc379f747c1d540d8043e86c7fa86
Justin Mancherje <mancherje.justin@gmail.com>
2016-06-06 21:48:40 -0700

    update responsiveness of ImageCards with a grid format

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -5,11 +5,19 @@ const ImageCard = ({
   imageData,
   portrait
 }) => (
-  <figure className='image-card'>
-    <img className={portrait ? 'portrait' : 'landscape'} 
-         src={imageData.media ? imageData.media.m : imageData.url} 
-         onClick={viewImage.bind(this, imageData)} /> 
-  </figure>
+  <div className='box'>
+    <div className="innerContent">
+      <img className={portrait ? 'portrait' : 'landscape'}
+           src={imageData.media ? imageData.media.m : imageData.url} 
+           onClick={viewImage.bind(this, imageData)} /> 
+    </div>
+  </div>
 )
 
 export default ImageCard
+
+// <figure className='image-card'>
+//   <img className={portrait ? 'portrait' : 'landscape'} 
+//        src={imageData.media ? imageData.media.m : imageData.url} 
+//        onClick={viewImage.bind(this, imageData)} /> 
+// </figure>
