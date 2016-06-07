4c510edb2d13eaab0ef1603ef80f5d8ce8e2394e
Justin Mancherje <mancherje.justin@gmail.com>
2016-06-07 10:57:51 -0700

    update ImageCard classes for styling and add page size change functionality to listView

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,23 +1,31 @@
 import React from 'react'
 
+function formatTitle(string, size) {
+  // limit title length so it doesn't show before hover
+  let title = string;
+  if (!string) {
+    title = 'untitled'
+  } else if (string.length > size + 3) {
+    title = string.substring(0, size + 1) + '...'
+  }
+  return <p className="titleBox" style={{ maxHeight: '50px' }}>{title}</p>
+}
+
 const ImageCard = ({
   viewImage,
   imageData,
   portrait
-}) => (
-  <div className='box'>
-    <div className="innerContent">
-      <img className={portrait ? 'portrait' : 'landscape'}
-           src={imageData.media ? imageData.media.m : imageData.url} 
-           onClick={viewImage.bind(this, imageData)} /> 
+}) => {
+  return (
+    <div className='box'>
+      <div className="boxInner">
+        <img className={portrait ? 'portrait' : 'landscape'}
+             src={imageData.media ? imageData.media.m : imageData.url} 
+             onClick={viewImage.bind(this, imageData)} />
+        {formatTitle(imageData.title, 25)}
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default ImageCard
-
-// <figure className='image-card'>
-//   <img className={portrait ? 'portrait' : 'landscape'} 
-//        src={imageData.media ? imageData.media.m : imageData.url} 
-//        onClick={viewImage.bind(this, imageData)} /> 
-// </figure>
--- src/components/ListViews/ListView.js
+++ src/components/ListViews/ListView.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { updateFeed, changeCurrentImage, fetchFavorites } from '../../actions'
+import { 
+  updateFeed, 
+  changeCurrentImage, 
+  fetchFavorites,
+  changeFeedPageSize,
+  changeFavoritesPageSize } from '../../actions'
 import ImageCard from '../ImageCard'
 import Pagination from '../Pagination/Pagination'
 import { Link } from 'react-router'
@@ -25,7 +30,7 @@ class ListView extends Component {
   renderImages() {
     const page = this.props.pageNumber - 1
     if (this.props.fetching && this.props.images.length === 0) {
-      return <img src="./assets/flickr.gif" alt="flickr spinner" />
+      return <img src="./assets/flickr.gif" alt="flickr spinner" className="center-block" />
     } else if (this.props.images.length === 0 && this.props.location.pathname === '/favorites') {
       return <h4>You have no favorites yet! Go back to the <Link to="/feed">Feed</Link> and select some favorites!</h4>
     }
@@ -38,15 +43,30 @@ class ListView extends Component {
       )
   }
 
+  changePageSize(event, pageSize) {
+    event.preventDefault()
+    if (this.props.location.pathname === '/feed') {
+      return this.props.changeFeedPageSize(pageSize)
+    } else {
+      return this.props.changeFavoritesPageSize(pageSize)
+    }
+  }
+
   render() {
     return (
       <div className="list-view">
         <button onClick={this.props.updateFeed}>Fetch More</button>
-        <Pagination path={this.props.location.pathname} numberOfPages={this.props.images.length} />
+        <Pagination pageSize={this.props.pageSize} 
+                    path={this.props.location.pathname} 
+                    numberOfPages={this.props.images.length} 
+                    changePageSize={this.changePageSize.bind(this)} />
         <section className="wrap">
           {this.renderImages.call(this)}
         </section>
-        <Pagination path={this.props.location.pathname} numberOfPages={this.props.images.length} />
+        <Pagination pageSize={this.props.pageSize} 
+                    path={this.props.location.pathname} 
+                    numberOfPages={this.props.images.length} 
+                    changePageSize={this.changePageSize.bind(this)} />
       </div>
     )
   }
@@ -57,11 +77,11 @@ ListView.contextTypes = {
 }
 
 function mapStateToPropsFeed(state) {
-  console.log('state in feed', state)
   return {
     images: state.images.images,
     pageNumber: state.ui.feedPage,
-    fetching: state.images.fetching
+    fetching: state.images.fetching,
+    pageSize: state.images.pageSize
   }
 }
 
@@ -69,12 +89,13 @@ function mapStateToPropsFavorites(state) {
   return {
     images: state.favorites.images,
     pageNumber: state.ui.favoritesPage,
-    fetching: state.favorites.fetching
+    fetching: state.favorites.fetching,
+    pageSize: state.favorites.pageSize
   }
 }
 
-const Feed = connect(mapStateToPropsFeed, { updateFeed, changeCurrentImage })(ListView)
-const Favorites = connect(mapStateToPropsFavorites, { changeCurrentImage, fetchFavorites })(ListView)
+const Feed = connect(mapStateToPropsFeed, { updateFeed, changeCurrentImage, changeFeedPageSize })(ListView)
+const Favorites = connect(mapStateToPropsFavorites, { changeCurrentImage, fetchFavorites, changeFavoritesPageSize })(ListView)
 
 export {
   Feed,
