f81b1af9c40fde54286bc27cef7d2f3e731b3597
Justin Mancherje <mancherje.justin@gmail.com>
2016-06-07 11:39:21 -0700

    update test for favorites and feed reducer to test for page size changes and toggling fetching true/false

--- test/reducers/favorites_reducer.js
+++ test/reducers/favorites_reducer.js
@@ -1,60 +1,154 @@
 import { expect } from 'chai'
 
 import reducer from '../../src/reducers/favorites_reducer'
-import { FETCH_FAVORITES } from '../../src/actions/types'
+import { 
+  FETCH_FAVORITES, 
+  FETCH_FAVORITES_REQUEST,
+  CHANGE_FAVORITES_PAGE_SIZE,
+  SPLIT_FAVORITES_PAGES } from '../../src/actions/types'
 
 
 describe('favorites reducer', () => {
 
-  it('handles initial FETCH_FAVORITES starting with empty state', () => {
-    const favorites = [{ title: 'image3' }, { title: 'image4' }]
+  it('handles unknown action types', () => {
     const initialState = { 
       images: [],
       pageSize: 4,
       fetching: false
     }
-    const action = { type: FETCH_FAVORITES, payload: { data: favorites } }
+    const action = { type: 'unknown' }
 
     const nextState = reducer(initialState, action)
 
     expect(nextState).to.deep.equal({ 
-      images: [[{ title: 'image3' }, { title: 'image4' }]],
+      images: [],
       pageSize: 4,
       fetching: false
     })
+
+
   })
 
-  it('handles subsequent FETCH_FAVORITES, and overwrite existing favorites', () => {
-    const favorites = [{ title: 'image3' }, { title: 'image4' }, { title: 'image5' }]
-    const initialState = { 
-      images: [[{ title: 'image3' }, { title: 'image4' }]],
-      pageSize: 4,
-      fetching: false
-    }
-    const action = { type: FETCH_FAVORITES, payload: { data: favorites } }
-    const nextState = reducer(initialState, action)
+  describe('FETCH_FAVORITES', () => {
+    it('handles initial FETCH_FAVORITES starting with empty state', () => {
+      const favorites = [{ title: 'image3' }, { title: 'image4' }]
+      const initialState = { 
+        images: [],
+        pageSize: 4,
+        fetching: false
+      }
+      const action = { type: FETCH_FAVORITES, payload: { data: favorites } }
 
-    expect(nextState).to.deep.equal({ 
-      images: [[{ title: 'image3' }, { title: 'image4' }, { title: 'image5' }]],
-      pageSize: 4,
-      fetching: false
+      const nextState = reducer(initialState, action)
+
+      expect(nextState).to.deep.equal({ 
+        images: [[{ title: 'image3' }, { title: 'image4' }]],
+        pageSize: 4,
+        fetching: false
+      })
+    })
+
+    it('handles subsequent FETCH_FAVORITES, and overwrite existing favorites', () => {
+      const favorites = [{ title: 'image3' }, { title: 'image4' }, { title: 'image5' }]
+      const initialState = { 
+        images: [[{ title: 'image3' }, { title: 'image4' }]],
+        pageSize: 4,
+        fetching: false
+      }
+      const action = { type: FETCH_FAVORITES, payload: { data: favorites } }
+      const nextState = reducer(initialState, action)
+
+      expect(nextState).to.deep.equal({ 
+        images: [[{ title: 'image3' }, { title: 'image4' }, { title: 'image5' }]],
+        pageSize: 4,
+        fetching: false
+      })
+    })  
+
+    it('returns an empty, unnested array if NO favorites are present', () => {
+      const favorites = []
+      const initialState = {
+        images: [],
+        pageSize: 10,
+        fetching: false
+      }
+      const action = { type: FETCH_FAVORITES, payload: { data: favorites } }
+      const nextState = reducer(initialState, action)
+
+      expect(nextState).to.deep.equal({
+        images: [],
+        pageSize: 10,
+        fetching: false
+      })
     })
   })
 
-  it('returns an empty, unnested array if NO favorites are present', () => {
-    const favorites = []
-    const initialState = {
-      images: [],
-      pageSize: 10,
-      fetching: false
-    }
-    const action = { type: FETCH_FAVORITES, payload: { data: favorites } }
-    const nextState = reducer(initialState, action)
+  describe('FETCH_FAVORITES_REQUEST', () => {
+    it('changes fetching status to true', () => {
+      const initialState = { 
+        images: [[{ title: 'image1' }, { title: 'image2' }]],
+        pageSize: 3,
+        fetching: false
+      }
 
-    expect(nextState).to.deep.equal({
-      images: [],
-      pageSize: 10,
-      fetching: false
+      const action = { type: FETCH_FAVORITES_REQUEST }
+      const nextState = reducer(initialState, action)
+
+      expect(nextState).to.deep.equal({ 
+        images: [[{ title: 'image1' }, { title: 'image2' }]],
+        pageSize: 3,
+        fetching: true
+      })
+    })
+  })
+
+  describe('CHANGE_FAVORITES_PAGE_SIZE', () => {
+    it('changes the curreng feed page size', () => {
+      const initialState = { 
+        images: [[{ title: 'image1', link: 'http://google.com/image1' }, { title: 'image2', link: 'http://google.com/image2' }]],
+        pageSize: 3,
+        fetching: false
+      }
+
+      const action = { type: CHANGE_FAVORITES_PAGE_SIZE, newSize: 20 }
+      const nextState = reducer(initialState, action)
+
+      expect(nextState).to.deep.equal({ 
+        images: [[{ title: 'image1', link: 'http://google.com/image1' }, { title: 'image2', link: 'http://google.com/image2' }]],
+        pageSize: 20,
+        fetching: false
+      })
+    })
+  })
+
+  describe('SPLIT_FAVORITES_PAGES', () => {
+    it('splits the images state into pages depending on the current pageSize', () => {
+      const initialState = { 
+        images: [[
+          { title: 'image1', link: 'http://google.com/image1' }, 
+          { title: 'image2', link: 'http://google.com/image2' },
+          { title: 'image3', link: 'http://google.com/image3' },
+          { title: 'image4', link: 'http://google.com/image4' },
+          { title: 'image5', link: 'http://google.com/image5' }
+        ]],
+        pageSize: 3,
+        fetching: false
+      }
+
+      const action = { type: SPLIT_FAVORITES_PAGES }
+      const nextState = reducer(initialState, action)
+
+      expect(nextState).to.deep.equal({ 
+        images: [[
+          { title: 'image1', link: 'http://google.com/image1' }, 
+          { title: 'image2', link: 'http://google.com/image2' },
+          { title: 'image3', link: 'http://google.com/image3' }],
+          [{ title: 'image4', link: 'http://google.com/image4' },
+          { title: 'image5', link: 'http://google.com/image5' }
+        ]],
+        pageSize: 3,
+        fetching: false
+      })
     })
   })
 })
--- test/reducers/images_reducer.js
+++ test/reducers/images_reducer.js
@@ -1,52 +1,15 @@
 import { expect } from 'chai'
 
 import reducer from '../../src/reducers/images_reducer'
-import { FETCH_IMAGES } from '../../src/actions/types'
+import { 
+  FETCH_IMAGES, 
+  FETCH_IMAGES_REQUEST, 
+  TOGGLE_FEED_FAVORITE, 
+  CHANGE_FEED_PAGE_SIZE, 
+  SPLIT_FEED_PAGES } from '../../src/actions/types'
 
 
 describe('images reducer', () => {
-
-  it('formats initial fetch correctly', () => {
-    const flickrData = {
-      title: "dummy title",
-      items: [{ title: 'image1' }, { title: 'image2' }]
-    }
-    const initialState = {
-      images: [],
-      pageSize: 5,
-      fetching: false
-    }
-    const action = { type: FETCH_IMAGES, payload: flickrData }
-
-    const nextState = reducer(initialState, action)
-
-    expect(nextState).to.deep.equal({
-      images: [[{ title: 'image1' }, { title: 'image2' }]],
-      pageSize: 5,
-      fetching: false
-    })
-  })
-
-  it('formats subsequent fetch calls into separate pages if there are more items than page size', () => {
-    const flickrData = {
-      title: "another dummy title",
-      items: [{ title: 'image3' }, { title: 'image4' }]
-    }
-    const initialState = { 
-      images: [[{ title: 'image1' }, { title: 'image2' }]],
-      pageSize: 3,
-      fetching: false
-    }
-    const action = { type: FETCH_IMAGES, payload: flickrData }
-
-    const nextState = reducer(initialState, action)
-
-    expect(nextState).to.deep.equal({
-      images: [[{ title: 'image1' }, { title: 'image2' }, { title: 'image3' }], [{ title: 'image4' }]],
-      pageSize: 3,
-      fetching: false
-    })
-  })
 
   it('handles unknown/unrelated action types', () => {
     const initialState = []
@@ -56,5 +19,159 @@ describe('images reducer', () => {
 
     expect(nextState).to.deep.equal(initialState)
   })
+
+  describe('FETCH_IMAGES', () => {
+    it('formats initial fetch correctly', () => {
+      const flickrData = {
+        title: "dummy title",
+        items: [{ title: 'image1' }, { title: 'image2' }]
+      }
+      const initialState = {
+        images: [],
+        pageSize: 5,
+        fetching: false
+      }
+      const action = { type: FETCH_IMAGES, payload: flickrData }
+
+      const nextState = reducer(initialState, action)
+
+      expect(nextState).to.deep.equal({
+        images: [[{ title: 'image1' }, { title: 'image2' }]],
+        pageSize: 5,
+        fetching: false
+      })
+    })
+
+    it('formats subsequent fetch calls into separate pages if there are more items than page size', () => {
+      const flickrData = {
+        title: "another dummy title",
+        items: [{ title: 'image3' }, { title: 'image4' }]
+      }
+      const initialState = { 
+        images: [[{ title: 'image1' }, { title: 'image2' }]],
+        pageSize: 3,
+        fetching: false
+      }
+      const action = { type: FETCH_IMAGES, payload: flickrData }
+
+      const nextState = reducer(initialState, action)
+
+      expect(nextState).to.deep.equal({
+        images: [[{ title: 'image1' }, { title: 'image2' }, { title: 'image3' }], [{ title: 'image4' }]],
+        pageSize: 3,
+        fetching: false
+      })
+    })
+
+    it('changes fetching status to false when successfully fetched images', () => {
+      const flickrData = {
+        title: "another dummy title",
+        items: [{ title: 'image3' }, { title: 'image4' }]
+      }
+      const initialState = { 
+        images: [[{ title: 'image1' }, { title: 'image2' }]],
+        pageSize: 3,
+        fetching: true
+      }
+      const action = { type: FETCH_IMAGES, payload: flickrData }
+
+      const nextState = reducer(initialState, action)
+
+      expect(nextState).to.deep.equal({
+        images: [[{ title: 'image1' }, { title: 'image2' }, { title: 'image3' }], [{ title: 'image4' }]],
+        pageSize: 3,
+        fetching: false
+      })
+    })
+  })
+
+  describe('FETCH_IMAGES_REQUEST', () => {
+    it('changes fetching status to true', () => {
+      const initialState = { 
+        images: [[{ title: 'image1' }, { title: 'image2' }]],
+        pageSize: 3,
+        fetching: false
+      }
+
+      const action = { type: FETCH_IMAGES_REQUEST }
+      const nextState = reducer(initialState, action)
+
+      expect(nextState).to.deep.equal({ 
+        images: [[{ title: 'image1' }, { title: 'image2' }]],
+        pageSize: 3,
+        fetching: true
+      })
+    })
+  })
+
+  describe('TOGGLE_FEED_FAVORITE', () => {
+    it('toggles favorite: true on correct image', () => {
+      const initialState = { 
+        images: [[{ title: 'image1', link: 'http://google.com/image1' }, { title: 'image2', link: 'http://google.com/image2' }]],
+        pageSize: 3,
+        fetching: false
+      }
+
+      const action = { type: TOGGLE_FEED_FAVORITE, image: { title: 'image2', link: 'http://google.com/image2' } }
+      const nextState = reducer(initialState, action)
+
+      expect(nextState).to.deep.equal({ 
+        images: [[{ title: 'image1', link: 'http://google.com/image1' }, { title: 'image2', link: 'http://google.com/image2', favorite: true }]],
+        pageSize: 3,
+        fetching: false
+      })
+    })
+  })
+
+  describe('CHANGE_FEED_PAGE_SIZE', () => {
+    it('changes the curreng feed page size', () => {
+      const initialState = { 
+        images: [[{ title: 'image1', link: 'http://google.com/image1' }, { title: 'image2', link: 'http://google.com/image2' }]],
+        pageSize: 3,
+        fetching: false
+      }
+
+      const action = { type: CHANGE_FEED_PAGE_SIZE, newSize: 20 }
+      const nextState = reducer(initialState, action)
+
+      expect(nextState).to.deep.equal({ 
+        images: [[{ title: 'image1', link: 'http://google.com/image1' }, { title: 'image2', link: 'http://google.com/image2' }]],
+        pageSize: 20,
+        fetching: false
+      })
+    })
+  })
+
+  describe('SPLIT_FEED_PAGES', () => {
+    it('splits the images state into pages depending on the current pageSize', () => {
+      const initialState = { 
+        images: [[
+          { title: 'image1', link: 'http://google.com/image1' }, 
+          { title: 'image2', link: 'http://google.com/image2' },
+          { title: 'image3', link: 'http://google.com/image3' },
+          { title: 'image4', link: 'http://google.com/image4' },
+          { title: 'image5', link: 'http://google.com/image5' }
+        ]],
+        pageSize: 3,
+        fetching: false
+      }
+
+      const action = { type: SPLIT_FEED_PAGES }
+      const nextState = reducer(initialState, action)
+
+      expect(nextState).to.deep.equal({ 
+        images: [[
+          { title: 'image1', link: 'http://google.com/image1' }, 
+          { title: 'image2', link: 'http://google.com/image2' },
+          { title: 'image3', link: 'http://google.com/image3' }],
+          [{ title: 'image4', link: 'http://google.com/image4' },
+          { title: 'image5', link: 'http://google.com/image5' }
+        ]],
+        pageSize: 3,
+        fetching: false
+      })
+    })
+  })
+  
 })
 
